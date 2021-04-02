9c6a539ff977a0bce04692c10226221cbfb630bc
aby <abybabu5@gmail.com>
2021-04-02 12:49:29 +0200

    next.js test

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
